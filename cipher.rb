3badaaf010977b3e39d40b3b9675fb3cc3dfcff9
Aleksandra <aleksandra.katic99@gmail.com>
2021-06-30 11:37:29 +0200

    Add methods

--- cipher.rb
+++ cipher.rb
@@ -1,6 +1,25 @@
-def rot_cipher(string)
-    rot_string = ""
+def rot13(char)
+    case char
+    when 'a'..'m', 'A'..'M'
+        char.ord + 13
+    when 'n'..'z', 'N'..'Z'
+        char.ord - 13
+    else
+        char.ord
+    end.chr
+end
+
+def rot_word(string)
+    new_string = string.chars.map do |char|
+        rot13(char)
+    end.join
+end
+
+def rot13_message(messages)
+    messages.map do |message|
+    end
 end
 
 puts "Enter a sentence: "
-text = gets.chomp
+message = gets.chomp
+puts rot_word(message)
